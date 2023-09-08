2a47854a256c5fffd71d7c464dcd892a1897da1f
Mukund K <mukund.kri@gmail.com>
2023-09-08 21:42:41 +0530

    getting started code

---
+++ 01_getting_started.js
@@ -0,0 +1,22 @@
+/*
+ * The hello world of sequelize. Here we connect to the database and validate the
+ * connection by logging the result to the console.
+ * That's it! in subsequent examples we will see how to create tables, insert data,
+ * and query data.
+ */
+
+// Imports
+const Sequelize = require('sequelize');
+
+// Connect to a sqlite database. 
+// The database is created in the file `database.sqlite` in the current folder.
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'database.sqlite'
+});
+
+// Test the connection by logging the result
+sequelize
+    .authenticate()
+    .then(() => console.log('Connection has been established successfully.'))
+    .catch(err => console.error('Unable to connect to the database:', err));
